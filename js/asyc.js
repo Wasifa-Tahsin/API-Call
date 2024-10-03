50259cc507378f191b3e364a2aaa07f12fc7b4fe
Afia <arminakterafia@gmail.com>
2024-10-03 18:08:28 +0600

    practice

---
+++ js/asyc.js
@@ -0,0 +1,10 @@
+console.log(1)
+console.log(2)
+setTimeout(()=>{console.log('lazy logged')},4000)
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+console.log(4)
+function doSomething(){
+    console.log(3)
+}
