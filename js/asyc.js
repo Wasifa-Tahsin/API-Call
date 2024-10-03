e8ea135956b5dc4f69a63efcc08989cf88da8df9
Afia <arminakterafia@gmail.com>
2024-10-03 18:38:19 +0600

    'practice

--- js/asyc.js
+++ js/asyc.js
@@ -1,10 +1,42 @@
-console.log(1)
-console.log(2)
-setTimeout(()=>{console.log('lazy logged')},4000)
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//       .then(response => response.json())
-//       .then(json => console.log(json))
-console.log(4)
-function doSomething(){
-    console.log(3)
+// console.log(1)
+// console.log(2)
+// setTimeout(()=>{console.log('lazy logged')},4000)
+// console.log(4)
+// function doSomething(){
+//     console.log(3)
+// }
+
+
+const myLoader=()=>{
+    return new Promise((resolve,reject)=>{
+        const success=Math.random();
+        if(success<=0.5){
+            resolve(success)
+        }
+        else{
+            reject(success)
+        }
+    })
 }
+myLoader()
+.then(data=>console.log('resolved', data))
+.catch(err=>console.log('rejected',err))
+
+
+fetch('')
+.then(res=>res.json())
+.then(data=>console.log(data))
+.catch(err=>console.log(err))
+
+// function declare er somoy avabe
+ async function loadData(){
+    const res=await fetch('')
+    const data=await res.json()
+    console.log(data)
+}
+
+// arrow function declare er somoy avabe
+const taskLoader=async()=>{
+    const res=await fetch('')
+}
+loadData()
